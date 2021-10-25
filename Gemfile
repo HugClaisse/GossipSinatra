28532b5d013414a0b94a28576dd41709d99a9e61
HugClaisse <freehugh92@gmail.com>
2021-10-25 20:37:33 +0200

    gossip

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+ruby '2.7.4'
+gem 'shotgun'
+gem 'sinatra'
+gem 'pry'
+gem 'csv'
---
+++ lib/gossip.rb
@@ -0,0 +1,21 @@
+class Gossip
+    attr_accessor :author, :content
+  
+    def initialize(content, author)
+      @content = content
+      @author = author
+    end
+   #Ajoute un nouveau gossip
+    def save
+      CSV.open('./db/gossip.csv', 'ab') do |csv|
+        csv << [@content, @author]
+      end
+    end
+    def self.all
+    all_gossips = []
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+  end
